be80fb190fa2ecb468173866ca0fd4a099110fec
Lee Hewitt <lhtechnicalsolutions@icloud.com>
2015-05-01 20:04:57 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= '48nYBEUXzzjX2grdWUqGspDzbbGLWCwEphPA3C5mrc38WiE2', // Unique master Xively API key to be used as a default
 		defaultFeeds	= ['2006218788'], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'LH Technical <=> Albany Mill', // Replaces Xively logo in the header
+		applicationName	= 'LH Technical Solutions Live Data Feeds', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 1080, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
